3ec1a84adadf335a126a56edf6698bcfa63d1f09
Levente Vig <vig.levente@codeyard.eu>
2019-09-21 11:44:42 +0200

    Initial Commit

---
+++ Template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Template
+//
+//  Created by Levente Vig on 2019. 09. 21..
+//  Copyright © 2019. levivig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
